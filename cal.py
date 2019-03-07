c7b79c8f552403fad29c0923fa313956a80f692a
Jonas Andersen <jonas@anrsn.me>
2019-03-07 17:04:33 +0100

    More work on cal.py>

--- cal.py
+++ cal.py
@@ -1,8 +1,38 @@
+import re
+import config
+import moodle_cal
+
 
 def getModifiedCalendar():
   return 'TODO'
 
 
 
-def parseDescription(eventdescription):
+def parseEventSummary(eventsummary):
   regex = r'([A-Za-z]+): (.+?)(?= - [A-Z]|$)'
+  objdict = {}
+  listmatch = re.findall(regex, eventsummary)
+  if listmatch == None:
+    return None
+  for element in listmatch:
+    objdict[element[0]] = element[1]
+  return objdict
+
+
+def getNewDescription(originaleventdescription, note, teacher):
+  array = []
+  if note != None:
+    array.append(note)
+  if teacher != None:
+    array.append(teacher)
+  if originaleventdescription != None:
+    array.append(originaleventdescription)
+  return str.join("\n", array)
+
+def getNewLocation(orignallocation, newlocation):
+  if newlocation == None:
+    return orignallocation
+  return newlocation
+
+def getNewName(originalname, courseid):
+  return config.getName(originalname, courseid)
